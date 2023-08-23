430ffe756c4a089a5ce53711e21b8ec41605a606
emmanuel-njihia <emmanuel.nduan@gmail.com>
2023-08-23 00:47:07 -0700

    new

--- linked_list.c
+++ linked_list.c
@@ -51,4 +51,77 @@ void free_stack(stack_t *custom_stack)
 }
 
 /**
- * que
+ * queue_node - node added to stack_t in mode queue
+ * @custom_stack: head stack
+ * @n: number node
+ *
+ * Return: new created node or NULL if allocated memory fails
+ */
+
+stack_t *queue_node(stack_t **custom_stack, const int n)
+
+{
+	stack_t *new = malloc(sizeof(stack_t));
+	stack_t *current = *custom_stack;
+
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->n = n;
+
+	if (!custom_stack)
+	{
+		new->next = NULL;
+		new->prev = NULL;
+		*custom_stack = new;
+		return (new);
+	}
+
+	while (current)
+	{
+		if (!current->next)
+		{
+			new->next = NULL;
+			new->prev = current;
+			current->next = new;
+			break;
+		}
+		current = current->next;
+
+	}
+
+	return (new);
+}
+
+/**
+ * add_node - start node added to stack_t stack
+ * @custom_stack - head stack
+ * @n: number of new node
+ *
+ * Return: node created newly, or NULL if creation fails
+ */
+
+stack_t *add_node(stack_t **custom_stack, const int n)
+
+{
+	stack_t *new = malloc(sizeof(stack_t));
+
+	if (!new)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free(new);
+		return (NULL);
+	}
+
+	new->n = n;
+
+	new->next = *custom_stack;
+	new->prev = NULL;
+	if (*custom_stack)
+		(*custom_stack)->prev = new;
+	*custom_stack = new;
+
+	return (new);
+}
