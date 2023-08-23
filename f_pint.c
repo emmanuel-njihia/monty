40cfdb9b0d61692ba142e3559b4f372b15d831d2
mutaimutai <mutaicaleb7539@gmail.com>
2023-08-23 00:52:47 -0700

    adding pint

---
+++ f_pint.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+/**
+ * pint - prints what is in the top of the stack
+ * @stack:where to retrieve the top element
+ * @count:number of line
+ */
+void pint(stack_t stack, unsigned in count)
+{
+	if (!stack || !(stack))
+	{
+		fprint(stderr, "L%d: can't pint, stack empty\n", count);
+		exit(EXIT_FAILURE):
+	}
+	printf("%d\n", (*stack)->n);
+}
