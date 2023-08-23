d54f086487ec90a1bf9d8a3831e8533e1f30c763
mutaimutai <mutaicaleb7539@gmail.com>
2023-08-23 00:42:38 -0700

    push pall functions

---
+++ f_pall.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * pall - prints all the elements in the stack starting from top
+ * @stack: ponter to the head of the stack
+ * @count: the number of lines
+ */
+void pall(stack_t **stack, unsigned int count)
+{
+	stack_t *head = element->head;
+
+	(void) stack;
+	(void) line_number;
+	if (head == NULL)
+		return;
+	while (head)
+	{
+		printf("%d\n", head->n);
+		head = head->next;
+	}
+}
