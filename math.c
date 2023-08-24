ba707e20d776d1bac8f0a72f9ff31016e84efa4f
emmanuel-njihia <emmanuel.nduan@gmail.com>
2023-08-24 03:43:38 -0700

    old to new

--- math.c
+++ math.c
@@ -1,22 +1,19 @@
 #include "monty.h"
 
 /**
- * add - top two elements added
- * @stack: top stack ptr
- * @line_number: command number line executed
- *
+ * add - adds the top two elements of the stack
+ * @stack: ptr to the top of the stack
+ * @line_number: the line number of the command being execute
  * Return: void
  */
-
 void add(stack_t **stack, unsigned int line_number)
-
 {
 	(void) stack;
 	(void) line_number;
 
 	if (element->head != NULL && element->head->next != NULL)
 	{
-		element->head->next->n = element->head->n + element->head->n;
+		element->head->next->n = element->head->n + element->head->next->n;
 		pop(NULL, 0);
 	}
 	else
@@ -26,49 +23,44 @@ void add(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * sub - top two elements subtracted
- * @stack: top stack ptr
- * @line_number:command number line executed
- *
+ * sub - subtracts the top element of the stack from the second
+ * top element of the stack
+ * @stack: ptr to the top of the stack
+ * @line_number: the line number of the command being execute
  * Return: void
  */
-
 void sub(stack_t **stack, unsigned int line_number)
-
 {
 	(void) stack;
 	(void) line_number;
 
 	if (element->head != NULL && element->head->next != NULL)
 	{
-		element->head->next->n = element->head->n - element->head->n;
+		element->head->next->n = element->head->next->n - element->head->n;
 		pop(NULL, 0);
 	}
 	else
 	{
 		exit_function(9);
 	}
-	}
+}
 
 /**
- * divide - top two elements divided
- * @stack: top stack ptr
- * @line_number: command number line execy=uted
- *
+ * divide - divides the second top element of the stack by the
+ * top element of the stack
+ * @stack: ptr to the top of the stack
+ * @line_number: the line number of the command being execute
  * Return: void
  */
-
 void divide(stack_t **stack, unsigned int line_number)
-
 {
 	(void) stack;
 	(void) line_number;
-
 	if (element->head != NULL && element->head->next != NULL)
 	{
 		if (element->head->n == 0)
 			exit_function(13);
-		element->head->next->n = element->head->n / element->head->n;
+		element->head->next->n = element->head->next->n / element->head->n;
 		pop(NULL, 0);
 	}
 	else
@@ -78,22 +70,20 @@ void divide(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * mul - top two elements multiplied
- * @stack: top stack ptr
- * @line_number: command line executed
- *
+ * mul - multiplies the second top element of the stack
+ * with the top element of the stack
+ * @stack: ptr to the top of the stack
+ * @line_number: the line number of the command being execute
  * Return: void
  */
-
 void mul(stack_t **stack, unsigned int line_number)
-
 {
 	(void) stack;
 	(void) line_number;
 
 	if (element->head != NULL && element->head->next != NULL)
 	{
-		element->head->next->n = element->head->n * element->head->n;
+		element->head->next->n = element->head->n * element->head->next->n;
 		pop(NULL, 0);
 	}
 	else
@@ -103,24 +93,22 @@ void mul(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * mod - computes rest of division of top second element
- * @stack: top stack ptr
- * @line_number: command line executed
- *
+ * mod - computes the rest of the division of the second top element
+ * of the stack by the top element of the stack
+ * @stack: ptr to the top of the stack
+ * @line_number: the line number of the command being execute
  * Return: void
  */
-
 void mod(stack_t **stack, unsigned int line_number)
-
 {
 	(void) stack;
 	(void) line_number;
 
-	if (element->head != NULL && element->head->next->n != NULL)
+	if (element->head != NULL && element->head->next != NULL)
 	{
 		if (element->head->n == 0)
 			exit_function(13);
-		element->head->next->n = element->head->n % element->head->n;
+		element->head->next->n = element->head->next->n % element->head->n;
 		pop(NULL, 0);
 	}
 	else
